928bb168217f9afa58bf2e926ea0813f0e544586
max beizer <max.beizer@gmail.com>
2019-01-20 10:06:48 -0600

    Set up for local deploy
    Digest assets with webpack and release as a one-liner
    ```
    cd assets && ./node_modules/webpack/bin/webpack.js --mode production &&
    cd .. && MIX_ENV=prod mix do phx.digest, release --env=prod
    ```

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,16 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :distill, DistillWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
+  # url: [host: "example.com", port: 80],
+  # cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}],
+  # This is critical for ensuring web-sockets properly authorize.
+  url: [host: "localhost", port: {:system, "PORT"}],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:distill, :vsn)
 
 # Do not print debug messages in production
 config :logger, level: :info
